668fe3535f692e69dff0d301fb79e315bc897386
tunewriter <100248269+tunewriter@users.noreply.github.com>
2022-10-23 02:27:03 +0200

    config change

--- frontend/app/cypress.config.js
+++ frontend/app/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     supportFile: false,
-    specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'e2e/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
